42ed4f59b057470b5ffb9d4a4d3f41a7d752fa71
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-25 15:08:50 +0200

    nging conf

--- data/install.sh
+++ data/install.sh
@@ -2,13 +2,14 @@
 
 echo "Running install.sh for $1"
 
-# TODO: Download selected version via  (from docker-sho-manager) from githup.com/magento/magento2 and run installer
-
-echo "Cloning magento repo"
-git clone --branch $1 --depth 1 https://$2:x-oauth-basic@github.com/shopware/shopware.git /www
+echo "Cloning shopware5 repo"
+git clone --branch $1 --depth 1 https://github.com/shopware/shopware.git /www
 
 echo "Installing composer packages"
-cd /www && composer.phar self-update && composer.phar config --global github-oauth.github.com $2 && composer.phar update --ignore-platform-reqs
+cd  /www \
+    && composer.phar self-update \
+    && composer.phar install --ignore-platform-reqs
+
 
 echo "Setting filesystem permissions"
 chown -R www-data /www
