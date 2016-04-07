3a1da7f8c6fe1d0e2ea9e6ec911107961ffdb27f
Mircea Silviu Itu <decemvre@gmail.com>
2016-04-07 22:54:46 +0300

    more installation

--- data/install.sh
+++ data/install.sh
@@ -8,8 +8,12 @@ git clone --branch $1 --depth 1 https://github.com/shopware/shopware.git /www
 echo "Installing composer packages"
 cd  /www \
     && composer.phar self-update \
-    && composer.phar install --ignore-platform-reqs
-
+    && composer.phar install --ignore-platform-reqs \
+    && mv config.php.dist config.php
+    
+cd  /www/recovery/common \
+    && composer.phar install --ignore-platform-reqs \
+    && composer.phar update
 
 echo "Setting filesystem permissions"
 chown -R www-data /www
